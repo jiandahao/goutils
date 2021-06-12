327aa34a23d5b01001dc2f44816f352f2a298b1f
jiandahao <779609717@qq.com>
2021-06-12 12:06:46 +0800

    add convjson example usage

---
+++ convjson/example/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/jiandahao/goutils/convjson"
+)
+
+func main() {
+	var data map[string]interface{}
+	if err := json.Unmarshal([]byte(`
+	{
+		"name": "tom",
+		"age": 27,
+		"gender": "male",
+		"extras": [
+			{
+				"city": "beijing",
+				"countryCode": "CN",
+				"cotunry": "china"
+			},
+			[
+				{
+					"hobbies": ["playing game", "swiming", "watching movies"]
+				}
+			]
+		],
+		"scores": {
+			"math": 98,
+			"history": 100,
+			"chemistry": 99
+		}
+	}
+	`), &data); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	val := convjson.NewValue(data)
+
+	name, _ := val.Get("name")
+	age, _ := val.Get("age")
+	city, _ := val.Get("extras[0].city")
+	hobbies, _ := val.Get("extras[1][0].hobbies")
+
+	mathScore, _ := val.Get("scores")
+	fmt.Println("name", name.MustString())
+	fmt.Println("age", age.MustInt())
+	fmt.Println("city", city.MustString())
+	fmt.Println("hobbies", hobbies.MustString())
+	fmt.Println("math scores", mathScore.MustInt())
+}
